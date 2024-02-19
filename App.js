64feb112dc5e6376329d8f12d91a2d9bee45a9c0
eeva <t2roee00@students.oamk.fi>
2024-02-19 09:39:29 +0200

    tyylit_pielessa

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Position from './components/Position';
+import Weather from './components/Weather';
+
+
+
+// const URL = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'
+// const APIKEY = 'd2b9a25438ee6f8cb8acb14777d09960'
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style ={styles.heading}>Current weather</Text>
+      <Position />
     </View>
   );
 }
@@ -18,3 +26,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+//units, vaihda yksiköt metreiksi yms
+
+
---
+++ components/Weather.js
@@ -0,0 +1,62 @@
+import { View, Text, Image } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import Position from './Position'
+import { StyleSheet } from 'react-native'
+
+const api = {
+url: process.env.EXPO_PUBLIC_API_URL,
+key: process.env.EXPO_PUBLIC_API_KEY,
+icons: process.env.EXPO_PUBLIC_ICONS_URL
+ /*  */
+}
+
+export default function Weather(lat,lon) {
+  const [temp, setTemp] = useState('')
+  const [description, setDescription] = useState('')
+  const [icon, setIcon] = useState('')
+
+  useEffect(() => {
+    const url = api.url +
+      'lat=' + lat +
+      '&lon=' + lon +
+      '&units=metric' +
+      '&lang=fi' +
+      '&appid=' + api.key
+
+    fetch(url)
+      .then(response => response.json())
+      .then((json) => {
+        console.log(json)
+        setTemp(json.main.temp)
+        setDescription(json.weather[0].description)
+        setIcon(api.icons + json.weather[0].icon + '@2x.png')
+      }).catch((error) => {
+        setDescription("Error retrieving weather information.")
+        console.log(error)
+      })
+  }, [])
+
+
+  return (
+    <View>
+      <Text style={styles.temp}>{temp}</Text>
+      {icon &&
+      <Image source={{uri: icon}} style= {{width:100, height: 100}}/>
+      }
+       <Text>{description}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      backgroundColor: '#FF0000',
+      padding: 5,
+  },
+  temp: {
+      flex: 1,
+      backgroundColor: '#FF0000',
+      padding: 5,
+  },
+});
